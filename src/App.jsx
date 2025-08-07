829dbb5b3d26d91f5132d613c4692e039b36a8a8
Ben Chapman <193661627+B-J-R-C@users.noreply.github.com>
2025-08-07 14:21:07 -0400

    to do list items not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 
 
-import React from 'react';
-//import './styles.css';
+
+
+import React, { useState } from 'react'; // Import useState hook
 import TodoList from './TodoList'; // Import TodoList
 import TodoForm from './TodoForm'; // Import TodoForm
 
+//import './styles.css';
+
 function App() {
+  // Creates new state value for new todo
+  const [newTodo, setNewTodo] = useState("Text  will be displayed when the component first renders");
+
   return (
     <div className="app-container">
       <div className="todo-app">
         <h1>Todo List</h1>
         <TodoForm />
+        
+        {/* paragraph element that displays the current value of the newTodo state variable */}
+        <p>{newTodo}</p>
+
         <TodoList />
+        
       </div>
     </div>
   );
