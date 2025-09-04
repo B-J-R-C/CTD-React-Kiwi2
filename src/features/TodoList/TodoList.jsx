7fdc078c479c4d5ab7b9aea9b56e050e2e7ad69a
Ben Chapman <193661627+B-J-R-C@users.noreply.github.com>
2025-09-03 20:28:46 -0400

    404 issue resolved

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -3,21 +3,24 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-function TodoList({ todoList, onCompleteTodo }) {
+function TodoList({ todoList, onCompleteTodo, isLoading }) {
+  if (isLoading) {
+    return <p>Todo list loading...</p>;
+  }
+
   const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
   return (
     <div>
       {filteredTodoList.length === 0 ? (
         <p>Add a todo above to get started</p>
       ) : (
-      <ul>
-        {filteredTodoList.map((todo) => (
-          // Pass the entire todo object, not just the title string
-          <TodoListItem key={todo.id} todo={todo} 
-          onCompleteTodo={onCompleteTodo}
-          />
-        ))}
-      </ul>
+        <ul>
+          {filteredTodoList.map((todo) => (
+            <TodoListItem key={todo.id} todo={todo} 
+            onCompleteTodo={onCompleteTodo}
+            />
+          ))}
+        </ul>
       )}
     </div>
   );
